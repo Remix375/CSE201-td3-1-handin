51544337258cb1b0a879756a693442f9953d3ac5
Remix375 <bigbossremi@gmail.com>
2023-10-12 11:53:23 +0200

    Update main.cpp
    online

--- main.cpp
+++ main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char* argv[])
   // START OF THE CUSTOM CODE SECTION
   // This code will be executed only if you set GRADING to a value different from 1
 
-    std::cout << "A better New initial message" << std::endl;
+    std::cout << "online message!" << std::endl;
 
   {
   }
